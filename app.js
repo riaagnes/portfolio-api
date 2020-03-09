116589d37a5fafb7d7f17fd4377cac7fb34b4e56
Ria agnes Jose <riaagnesjose@Rias-MacBook-Pro.local>
2020-03-09 13:34:10 -0700

    initial scaffolding

---
+++ app.js
@@ -0,0 +1,60 @@
+  
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const sendGrid = require('@sendGrid/mail');
+const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+app.get('/api', (req, res, next) => {
+    res.send('API Status: I\'m awesome')
+});
+
+
+app.post('/api/email', (req, res, next) => {
+
+    console.log(req.body);
+
+    sendGrid.setApiKey(process.env.API_KEY);
+    const msg = {
+        to: 'joseagnesria@gmail.com',
+        from: req.body.email,
+        subject: 'Website Contact',
+        text: req.body.message
+    }
+
+    sendGrid.send(msg)
+        .then(result => {
+
+            res.status(200).json({
+                success: true
+            });
+
+        })
+        .catch(err => {
+
+            console.log('error: ', err);
+            res.status(401).json({
+                success: false
+            });
+
+        });
+});
+
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
